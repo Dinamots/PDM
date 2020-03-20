af8d2a6e855f0b3f3ceca2e29da79afe6e3e5a30
Dinamots <jolan.daumas@gmail.com>
2020-03-20 11:40:18 +0100

    detail begin

--- tmdb/app/src/main/java/fr/m1miage/tmdb/ui/movie/MovieDetailFragment.kt
+++ tmdb/app/src/main/java/fr/m1miage/tmdb/ui/movie/MovieDetailFragment.kt
@@ -7,10 +7,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
+import com.squareup.picasso.Picasso
 
 import fr.m1miage.tmdb.R
 import fr.m1miage.tmdb.api.RetrofitManager
+import fr.m1miage.tmdb.api.model.Movie
+import fr.m1miage.tmdb.utils.TMDB_IMAGES_PATH
 import io.reactivex.android.schedulers.AndroidSchedulers
+import kotlinx.android.synthetic.main.movie_detail_fragment.*
 
 class MovieDetailFragment : Fragment() {
     val movieDetailViewModel: MovieDetailViewModel by activityViewModels()
@@ -29,10 +33,19 @@ class MovieDetailFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         movieDetailViewModel.movieId.observe(viewLifecycleOwner, Observer {
-           movieDetailViewModel.movie().observe(viewLifecycleOwner, Observer {
-
-           })
+            RetrofitManager.tmdbAPI.getMovie(it.toLong()).observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                { movie -> initView(movie) },
+                { error -> println(error) }
+            )
         })
+    }
+
+    private fun initView(movie: Movie) {
+        Picasso.get()
+            .load(TMDB_IMAGES_PATH + if (movie.poster_path !== "") movie.poster_path else movie.backdrop_path)
+            .fit()
+            .into(movie_img)
     }
 
 }
